jow-/umapd

commit f66a44ed6854d5a89c1537724ce272bebed016d9

treewide: convert to modules to import/export style
Utilize ucode's new ES6 style module system to link helper libraries at
compile time instead of importing them at runtime through `require()`.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

u1905/ubus.uc
@@ -14,8 +14,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-const defs = require('u1905.defs');
-const ubus = require('ubus');
+import defs from 'u1905.defs';
+import model from 'u1905.model';
+
+import { connect as ubus_connect, error as ubus_error } from 'ubus';
 
 let model = null;
 let ubusconn = null;
@@ -163,16 +165,15 @@ const I1905UbusProcedures = {
 	}
 };
 
-return {
+export default {
 	connect: function() {
-		ubusconn ??= ubus.connect();
-		model ??= require('u1905.model');
+		ubusconn ??= ubus_connect();
 
 		return (ubusconn != null);
 	},
 
 	error: function() {
-		return ubus.error();
+		return ubus_error();
 	},
 
 	publish: function() {

u1905.uc
@@ -17,16 +17,16 @@
 
 'use strict';
 
-const uloop = require('uloop');
+import * as uloop from 'uloop';
 
-const socket = require('u1905.socket');
-const cmdu = require('u1905.cmdu');
-const lldp = require('u1905.lldp');
-const utils = require('u1905.utils');
-const model = require('u1905.model');
-const defs = require('u1905.defs');
-const ubus = require('u1905.ubus');
-const log = require('u1905.log');
+import socket from 'u1905.socket';
+import cmdu from 'u1905.cmdu';
+import lldp from 'u1905.lldp';
+import utils from 'u1905.utils';
+import model from 'u1905.model';
+import defs from 'u1905.defs';
+import ubus from 'u1905.ubus';
+import log from 'u1905.log';
 
 function srcmac_to_almac(address) {
 	let i1905dev = model.lookupDevice(address);
@@ -277,7 +277,7 @@ function handle_lldp_input(flags) {
 function update_self() {
 	this.set(5000);
 
-	model.updateSelf();
+	model.updateSelf(ubus.call('network.interface', 'dump')?.interface ?? []);
 }
 
 function cleanup_model() {

u1905/cmdu.uc
@@ -14,14 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-const fs = require('fs');
-const rtnl = require('rtnl');
-const struct = require('struct');
-const nl80211 = require('nl80211');
-const i1905tlv = require('u1905.tlv');
-const utils = require('u1905.utils');
-const log = require('u1905.log');
-const defs = require('u1905.defs');
+import { pack, unpack } from 'struct';
+
+import i1905tlv from 'u1905.tlv';
+import utils from 'u1905.utils';
+import log from 'u1905.log';
+import defs from 'u1905.defs';
 
 const ETHERNET_HEADER_LENGTH = 14;
 const IEEE1905_HEADER_LENGTH = 8;
@@ -39,7 +37,7 @@ const CMDU_MAX_PAYLOAD_SIZE = 102400;
 let reassembly = utils.Queue(CMDU_MAX_CONCURRENT_REASSEMBLY);
 
 function send_fragment(socket, src, dest, type, mid, fid, payload_fmt, payload_data, flags) {
-	return socket.send(src, dest, struct.pack(`!BxHHBB${join('', payload_fmt)}`,
+	return socket.send(src, dest, pack(`!BxHHBB${join('', payload_fmt)}`,
 		CMDU_MESSAGE_VERSION,
 		type,
 		mid,
@@ -67,7 +65,7 @@ function parse_fragment(tlvs) {
 	return payload;
 }
 
-return {
+export default {
 	mid_counter: 0,
 
 	create: function(type, mid) {
@@ -85,7 +83,7 @@ return {
 			return null;
 		}
 
-		let pktdata = struct.unpack('!BxHHBB*', payload),
+		let pktdata = unpack('!BxHHBB*', payload),
 		    version = pktdata[0],
 		    type = pktdata[1],
 		    mid = pktdata[2],
@@ -232,7 +230,7 @@ return {
 			this.add_tlv(0);
 
 		for (let tlv in this.tlvs) {
-			let tlv_data = struct.pack('!BH*', tlv.type, tlv.length, tlv.payload);
+			let tlv_data = pack('!BH*', tlv.type, tlv.length, tlv.payload);
 			let tlv_length = length(tlv_data);
 
 			assert(tlv_length <= CMDU_MAX_PAYLOAD_LENGTH, 'TLV too large');

u1905/defs.uc
@@ -14,7 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-return {
+export default {
 	IEEE1905_MULTICAST_MAC: '01:80:c2:00:00:13',
 	LLDP_NEAREST_BRIDGE_MAC: '01:80:c2:00:00:0e',
 

u1905/lldp.uc
@@ -14,12 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-const struct = require('struct');
+import { pack, unpack } from 'struct';
 
-const utils = require('u1905.utils');
-const defs = require('u1905.defs');
+import utils from 'u1905.utils';
+import defs from 'u1905.defs';
 
-return {
+export default {
 	create: function(chassis, port, ttl) {
 		return proto({ chassis, port, ttl }, this);
 	},
@@ -29,7 +29,7 @@ return {
 		let offset = 0;
 
 		while (true) {
-			let tl = struct.unpack('!BB', payload, offset);
+			let tl = unpack('!BB', payload, offset);
 
 			if (!tl || (tl[0] == 0 && tl[1] == 0))
 				break;
@@ -41,19 +41,19 @@ return {
 				return null;
 
 			if (t == 0x1 && l == 7) {
-				let v = struct.unpack('!B6s', payload, offset + 2);
+				let v = unpack('!B6s', payload, offset + 2);
 
 				if (v?.[0] == 4)
 					chassis = utils.ether_ntoa(v[1]);
 			}
 			else if (t == 0x2 && l == 7) {
-				let v = struct.unpack('!B6s', payload, offset + 2);
+				let v = unpack('!B6s', payload, offset + 2);
 
 				if (v?.[0] == 3)
 					port = utils.ether_ntoa(v[1]);
 			}
 			else if (t == 0x3 && l == 2) {
-				let v = struct.unpack('!H', payload, offset + 2);
+				let v = unpack('!H', payload, offset + 2);
 
 				if (v)
 					ttl = v[0];
@@ -69,7 +69,7 @@ return {
 	},
 
 	send: function(socket) {
-		return socket.send(socket.address, defs.LLDP_NEAREST_BRIDGE_MAC, struct.pack('!3B6s 3B6s 2BH H',
+		return socket.send(socket.address, defs.LLDP_NEAREST_BRIDGE_MAC, pack('!3B6s 3B6s 2BH H',
 			(0x1 << 1), 7, 4,			// Chassis ID TLV, subtype 4 (LL address)
 			hexdec(this.chassis, ':'),	// Chassis ID TLV MAC
 

u1905/log.uc
@@ -14,7 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-return {
+export default {
 	debug: (fmt, ...args) => warn(sprintf(`[D] ${fmt}\n`, ...args)),
 	warn:  (fmt, ...args) => warn(sprintf(`[W] ${fmt}\n`, ...args)),
 	error: (fmt, ...args) => warn(sprintf(`[E] ${fmt}\n`, ...args)),

u1905/model.uc
@@ -14,18 +14,17 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-const nl80211 = require('nl80211');
-const struct = require('struct');
-const rtnl = require('rtnl');
-const fs = require('fs');
+import { request as wlrequest, 'const' as wlconst } from 'nl80211';
+import { request as rtrequest, 'const' as rtconst } from 'rtnl';
+import { unpack } from 'struct';
+import { readfile } from 'fs';
 
-const socket = require('u1905.socket');
-const utils = require('u1905.utils');
-const cmdu = require('u1905.cmdu');
-const tlv = require('u1905.tlv');
-const log = require('u1905.log');
-const ubus = require('u1905.ubus');
-const defs = require('u1905.defs');
+import socket from 'u1905.socket';
+import utils from 'u1905.utils';
+import cmdu from 'u1905.cmdu';
+import tlv from 'u1905.tlv';
+import log from 'u1905.log';
+import defs from 'u1905.defs';
 
 function timems() {
 	let tv = clock(true) ?? clock(false);
@@ -37,7 +36,7 @@ function resolve_bridge_ports(ifname) {
 	let upper = ifname;
 
 	while (true) {
-		link = rtnl.request(rtnl.const.RTM_GETLINK, 0, { dev: upper });
+		link = rtrequest(rtconst.RTM_GETLINK, 0, { dev: upper });
 
 		if (!link)
 			return null;
@@ -59,17 +58,17 @@ function resolve_bridge_ports(ifname) {
 	let links = [];
 
 	if (bridge) {
-		let bridge_links = rtnl.request(
-			rtnl.const.RTM_GETLINK,
-			rtnl.const.NLM_F_DUMP|rtnl.const.NLM_F_STRICT_CHK,
+		let bridge_links = rtrequest(
+			rtconst.RTM_GETLINK,
+			rtconst.NLM_F_DUMP|rtconst.NLM_F_STRICT_CHK,
 			{ master: bridge }
 		);
 
 		if (vlan) {
-			let bridge_vlans = rtnl.request(
-				rtnl.const.RTM_GETLINK,
-				rtnl.const.NLM_F_DUMP, {
-				family: rtnl.const.AF_BRIDGE,
+			let bridge_vlans = rtrequest(
+				rtconst.RTM_GETLINK,
+				rtconst.NLM_F_DUMP, {
+				family: rtconst.AF_BRIDGE,
 				ext_mask: 2
 			});
 
@@ -82,7 +81,7 @@ function resolve_bridge_ports(ifname) {
 						if (vi.vid > vlan || (vi.vid_end ?? vi.vid) < vlan)
 							continue;
 
-						if (vi.flags & rtnl.const.BRIDGE_VLAN_INFO_UNTAGGED)
+						if (vi.flags & rtconst.BRIDGE_VLAN_INFO_UNTAGGED)
 							push(links, { ifname: link.ifname, address: link.address });
 						else
 							push(links, { ifname: link.ifname, address: link.address, vlan });
@@ -229,7 +228,7 @@ const I1905LocalInterface = proto({
 		let info = this.getRuntimeInformation();
 
 		if (info.ipaddrs === null) {
-			let addrs = rtnl.request(rtnl.const.RTM_GETADDR, rtnl.const.NLM_F_DUMP|rtnl.const.NLM_F_STRICT_CHK, { dev: this.ifname, family: rtnl.const.AF_INET });
+			let addrs = rtrequest(rtconst.RTM_GETADDR, rtconst.NLM_F_DUMP|rtconst.NLM_F_STRICT_CHK, { dev: this.ifname, family: rtconst.AF_INET });
 			let ifstat;
 
 			for (let s in ifstatus?.interface) {
@@ -242,7 +241,7 @@ const I1905LocalInterface = proto({
 			info.ipaddrs = [];
 
 			for (let addr in addrs) {
-				if (addr.family != rtnl.const.AF_INET)
+				if (addr.family != rtconst.AF_INET)
 					continue;
 
 				let ip_mask_type_dhcp = split(addr.address, '/');
@@ -276,7 +275,7 @@ const I1905LocalInterface = proto({
 		let info = this.getRuntimeInformation();
 
 		if (info.ip6addrs === null) {
-			let addrs = rtnl.request(rtnl.const.RTM_GETADDR, rtnl.const.NLM_F_DUMP|rtnl.const.NLM_F_STRICT_CHK, { dev: this.ifname, family: rtnl.const.AF_INET6 });
+			let addrs = rtrequest(rtconst.RTM_GETADDR, rtconst.NLM_F_DUMP|rtconst.NLM_F_STRICT_CHK, { dev: this.ifname, family: rtconst.AF_INET6 });
 			let ifstat;
 
 			for (let s in ifstatus?.interface) {
@@ -289,7 +288,7 @@ const I1905LocalInterface = proto({
 			info.ip6addrs = [ [ '::', 0, 0, '::' ] ];
 
 			for (let addr in addrs) {
-				if (addr.family != rtnl.const.AF_INET6)
+				if (addr.family != rtconst.AF_INET6)
 					continue;
 
 				// skip expired addresses
@@ -331,7 +330,7 @@ const I1905LocalInterface = proto({
 				// On unavailable ubus state, try to guess SLAAC state */
 				if (ip_mask_type_origin[2] == 0 &&
 				    addr.cacheinfo.valid < 4294967295 /* address expires */ &&
-				    !(addr.flags & rtnl.const.IFA_F_PERMANENT) /* address is not permanent */) {
+				    !(addr.flags & rtconst.IFA_F_PERMANENT) /* address is not permanent */) {
 					if (ip_mask_type_origin[1] == 64)
 						ip_mask_type_origin[2] = 3; /* SLAAC */
 					else
@@ -377,10 +376,10 @@ const I1905LocalInterface = proto({
 			return this.info;
 
 		let ifname = this.i1905txsock.ifname,
-		    link = rtnl.request(rtnl.const.RTM_GETLINK, 0, { dev: ifname }),
-		    wifi = nl80211.request(nl80211.const.NL80211_CMD_GET_INTERFACE, 0, { dev: ifname }),
-		    wphy = nl80211.request(nl80211.const.NL80211_CMD_GET_WIPHY, 0, { dev: ifname }),
-		    wsta = nl80211.request(nl80211.const.NL80211_CMD_GET_STATION, nl80211.const.NLM_F_DUMP, { dev: ifname });
+		    link = rtrequest(rtconst.RTM_GETLINK, 0, { dev: ifname }),
+		    wifi = wlrequest(wlconst.NL80211_CMD_GET_INTERFACE, 0, { dev: ifname }),
+		    wphy = wlrequest(wlconst.NL80211_CMD_GET_WIPHY, 0, { dev: ifname }),
+		    wsta = wlrequest(wlconst.NL80211_CMD_GET_STATION, wlconst.NLM_F_DUMP, { dev: ifname });
 
 		if (!link)
 			return null;
@@ -390,8 +389,8 @@ const I1905LocalInterface = proto({
 			address: link.address,
 			statistics: link.stats64,
 			bridge: (link.linkinfo?.slave?.type == 'bridge') ? link.master : null,
-			speed: +fs.readfile(`/sys/class/net/${ifname}/speed`),
-			mtu: +fs.readfile(`/sys/class/net/${ifname}/mtu`),
+			speed: +readfile(`/sys/class/net/${ifname}/speed`),
+			mtu: +readfile(`/sys/class/net/${ifname}/mtu`),
 			wifi: (wifi && wphy) ? {
 				phy: wphy,
 				interface: wifi,
@@ -763,7 +762,7 @@ const I1905Device = proto({
 	}
 }, I1905Entity);
 
-return proto({
+export default proto({
 	address: '00:00:00:00:00:00',
 	interfaces: {},
 	devices: [],
@@ -780,7 +779,7 @@ return proto({
 				mac = i1905lif.address;
 
 		/* ... hash its bytes ... */
-		mac = struct.unpack('!6B', hexdec(mac, ':'));
+		mac = unpack('!6B', hexdec(mac, ':'));
 
 		hash = ((hash << 5) + hash) + mac[0];
 		hash = ((hash << 5) + hash) + mac[1];
@@ -804,7 +803,7 @@ return proto({
 		let vlan;
 
 		while (true) {
-			let link = rtnl.request(rtnl.const.RTM_GETLINK, 0, { dev: upper });
+			let link = rtrequest(rtconst.RTM_GETLINK, 0, { dev: upper });
 
 			if (!link)
 				return null;
@@ -887,7 +886,7 @@ return proto({
 		return [ ...this.devices ];
 	},
 
-	updateSelf: function() {
+	updateSelf: function(ifstatus) {
 		let i1905dev = this.addDevice(this.address);
 		let bridges = {};
 		let tlvs = [];
@@ -895,8 +894,7 @@ return proto({
 		let i1905neighs = [];
 		let i1905macs = [];
 
-		let neightbl = rtnl.request(rtnl.const.RTM_GETNEIGH, rtnl.const.NLM_F_DUMP) ?? [];
-		let ifstatus = ubus.call('network.interface', 'dump')?.interface ?? [];
+		let neightbl = rtrequest(rtconst.RTM_GETNEIGH, rtconst.NLM_F_DUMP) ?? [];
 
 		for (let i1905neigh in this.devices)
 			if (i1905neigh.isIEEE1905())
@@ -937,10 +935,10 @@ return proto({
 					if (neigh.dev != info.ifname)
 						continue;
 
-					if (neigh.type != rtnl.const.RTN_UNICAST)
+					if (neigh.type != rtconst.RTN_UNICAST)
 						continue;
 
-					if (neigh.state != rtnl.const.NUD_REACHABLE && neigh.state != rtnl.const.NUD_PERMANENT)
+					if (neigh.state != rtconst.NUD_REACHABLE && neigh.state != rtconst.NUD_PERMANENT)
 						continue;
 
 					if (!(neigh.lladdr in l2devs))
@@ -976,7 +974,7 @@ return proto({
 				push(tlvs, tlv.encode(defs.TLV_NON1905_NEIGHBOR_DEVICES, info.address, others));
 
 			if (l2devs)
-				push(tlvs, tlv.encode(defs.TLV_L2_NEIGHBOR_DEVICE, info.address, l2devs));
+				push(tlvs, tlv.encode(defs.TLV_L2_NEIGHBOR_DEVICE, info.address, l2devs, this.getDevices()));
 		}
 
 		for (let i1905neigh in i1905neighs) {
@@ -1007,7 +1005,7 @@ return proto({
 		push(tlvs,
 			tlv.encode(defs.TLV_IPV4, i1905lifs, ifstatus),
 			tlv.encode(defs.TLV_IPV6, i1905lifs, ifstatus),
-			tlv.encode(defs.TLV_DEVICE_INFORMATION, i1905lifs),
+			tlv.encode(defs.TLV_DEVICE_INFORMATION, i1905lifs, this.address),
 			tlv.encode(defs.TLV_DEVICE_IDENTIFICATION, null, null, null),
 			tlv.encode(defs.TLV_DEVICE_BRIDGING_CAPABILITY, values(bridges)),
 			tlv.encode(defs.TLV_CONTROL_URL, 'http://192.168.1.1' /* FIXME */),

u1905/socket.uc
@@ -14,12 +14,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-const struct = require('struct');
-const rtnl = require('rtnl');
+import { request as rtrequest, 'const' as rtconst } from 'rtnl';
+import { socket, error as sockerr } from 'u1905.socket.raw';
+import { pack } from 'struct';
 
-const rawsock = require('u1905.socket.raw');
-const utils = require('u1905.utils');
-const log = require('u1905.log');
+import utils from 'u1905.utils';
+import log from 'u1905.log';
 
 let err;
 
@@ -29,7 +29,7 @@ function failure(msg) {
 	return null;
 }
 
-return {
+export default {
 	const: {
 		ETH_P_8021Q: 0x8100,
 		ETH_P_LLDP:  0x88cc,
@@ -49,7 +49,7 @@ return {
 		let address, bridge;
 
 		while (true) {
-			let link = rtnl.request(rtnl.const.RTM_GETLINK, 0, { dev: upper });
+			let link = rtrequest(rtconst.RTM_GETLINK, 0, { dev: upper });
 
 			if (!link)
 				return failure('No such interface');
@@ -69,10 +69,10 @@ return {
 			break;
 		}
 
-		let sock = rawsock.socket(ifname, ethproto);
+		let sock = socket(ifname, ethproto);
 
 		if (!sock)
-			return failure(rawsock.error());
+			return failure(sockerr());
 
 		return proto({
 			address, ifname, bridge, vlan,
@@ -88,9 +88,9 @@ return {
 		    frame;
 
 		if (this.vlan)
-			frame = struct.pack('!6s6sHHH*', dmac, smac, this.const.ETH_P_8021Q, this.vlan, this.protocol, data);
+			frame = pack('!6s6sHHH*', dmac, smac, this.const.ETH_P_8021Q, this.vlan, this.protocol, data);
 		else
-			frame = struct.pack('!6s6sH*', dmac, smac, this.protocol, data);
+			frame = pack('!6s6sH*', dmac, smac, this.protocol, data);
 
 		return this.socket.send(dest, frame);
 	},
@@ -99,7 +99,7 @@ return {
 		let frame = this.socket.recv();
 
 		if (!frame)
-			return failure(rawsock.error());
+			return failure(sockerr());
 
 		let dstmac = utils.ether_ntoa(frame, 0);
 		let srcmac = utils.ether_ntoa(frame, 6);
@@ -111,7 +111,7 @@ return {
 
 		if (this.bridge) {
 			let search = {
-				family: rtnl.const.AF_BRIDGE,
+				family: rtconst.AF_BRIDGE,
 				master: this.bridge,
 				lladdr: srcmac
 			};
@@ -119,13 +119,13 @@ return {
 			if (this.vlan)
 				search.vlan = this.vlan;
 
-			let neigh = rtnl.request(rtnl.const.RTM_GETNEIGH, 0, search);
+			let neigh = rtrequest(rtconst.RTM_GETNEIGH, 0, search);
 
 			if (neigh) {
 				let rxdev = ifcname = neigh.dev;
 
 				if (!this.ports[rxdev]) {
-					let link = rtnl.request(rtnl.const.RTM_GETLINK, 0, { dev: rxdev });
+					let link = rtrequest(rtconst.RTM_GETLINK, 0, { dev: rxdev });
 
 					if (link)
 						ifcmac = this.ports[rxdev] = link.address;

commit 5027689cf541a53fbb85f0d429b90e1c4850ee44

ubus: fix model access
Remove an unused local variable `model` shadowing the imported data
model singleton.

Fixes: f66a44e ("treewide: convert to modules to import/export style")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

u1905/ubus.uc
@@ -19,7 +19,6 @@ import model from 'u1905.model';
 
 import { connect as ubus_connect, error as ubus_error } from 'ubus';
 
-let model = null;
 let ubusconn = null;
 
 const I1905UbusProcedures = {