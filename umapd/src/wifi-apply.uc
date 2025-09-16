jow-/umapd

commit d32b10eb81924b9a57c43099eab7f6271ec612be

wifi-apply: send service data with additional instance level
Since procd upstream commit dafdf98b ("service: fix object nesting in the
ubus get_data call") we must set the runtime wifi configuration per instance
for netifd and wifi-scripts to properly pick up the settings.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

umapd/src/wifi-apply.uc
@@ -85,7 +85,7 @@ const service_data = ubus.call('service', 'get_data', {
 })?.['umap-agent'];
 
 const new_instances = { [radio]: {} };
-const cur_instances = service_data?.['*']?.['wifi-iface'] ?? service_data?.['wifi-iface'] ?? {};
+const cur_instances = service_data?.['agent']?.['wifi-iface'] ?? {};
 
 for (let state_radio, state_bsses in cur_instances) {
 	if (state_radio != radio) {
@@ -189,7 +189,13 @@ for (let bss in sort(settings, bss_cmp)) {
 if (!equal(cur_instances, new_instances)) {
 	ubus.call('service', 'set', {
 		name: 'umap-agent',
-		data: { 'wifi-iface': new_instances }
+		instances: {
+			instance1: {
+				data: {
+					'wifi-iface': new_instances
+				}
+			}
+		}
 	});
 
 	ubus.call('network', 'reload');

umapd/files/umapd.agent.init
@@ -39,7 +39,7 @@ start_service() {
 		exit 1
 	}
 
-	procd_open_instance
+	procd_open_instance agent
 
 	procd_set_param respawn 30 3 0
 	procd_set_param stderr 1
@@ -53,29 +53,29 @@ start_service() {
 		v=$((v + 1))
 	done
 
-    for ifname in $devices; do
-        procd_append_param command --interface "$ifname"
-    done
+	for ifname in $devices; do
+		procd_append_param command --interface "$ifname"
+	done
 
 	for bridgedev in $bridgedevs; do
 		procd_append_param command --bridge "$bridgedev"
 	done
 
-    for radio in $radios; do
-        procd_append_param command --radio "$radio"
-    done
+	for radio in $radios; do
+		procd_append_param command --radio "$radio"
+	done
 
-	procd_close_instance
-}
-
-service_data() {
 	if [ -f /etc/umap-wireless-status.json ]; then
+		procd_open_data
 		json_add_object wifi-iface
 		eval "json_init() { :; }; $(jshn -R /etc/umap-wireless-status.json)"
 		json_close_object
+		procd_close_data
 
 		network_reload=1
 	fi
+
+	procd_close_instance
 }
 
 service_started() {

commit 59d2139ab0c18e1fc7ad2efade6fac64f13df534

wifi-apply: fix instance name in ubus set_data call
Fixes: d32b10e ("wifi-apply: send service data with additional instance level")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

umapd/src/wifi-apply.uc
@@ -190,7 +190,7 @@ if (!equal(cur_instances, new_instances)) {
 	ubus.call('service', 'set', {
 		name: 'umap-agent',
 		instances: {
-			instance1: {
+			agent: {
 				data: {
 					'wifi-iface': new_instances
 				}